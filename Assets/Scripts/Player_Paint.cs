71779e6add5317e951fb843d685acc9f51791307
tuckidart <tiagoduarte6@gmail.com>
2016-08-29 05:18:50 -0300

    troca de textura do carro na oficina

---
+++ Assets/Scripts/Player_Paint.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class Player_Paint : NetworkBehaviour {
+
+	public List<Texture> textureList;
+	private Dictionary<int, Texture> textures = new Dictionary<int, Texture> ();
+
+	[SyncVar (hook = "UpdateDisplayedTexture")]
+	public int currentTexture;
+
+	public GameObject carBody;
+
+	public bool canPaint;
+
+	void Start()
+	{
+		canPaint = true;
+		for(int i=0;i<textureList.Count;i++)
+		{
+			textures.Add (i, textureList [i]);
+		}
+	}
+		
+	void OnTriggerEnter(Collider hit)
+	{
+		if (isLocalPlayer)
+		{
+			if(hit.gameObject.name == "colliderToChangeTexture" && canPaint)
+			{
+				//aqui rola um sorteio atraves da lista de textura e escolhe uma aleatoria que não seja a textura atual
+				int randTex;
+				do
+					randTex = Random.Range (0, textureList.Count);
+				while
+					(randTex == currentTexture);
+					
+				CmdPaint(randTex);
+				canPaint = false;
+			}	
+		}
+	}
+
+	void OnTriggerExit()
+	{
+		canPaint = true;
+	}
+
+	void UpdateDisplayedTexture(int textureIndex)
+	{
+		carBody.GetComponent<Renderer>().material.mainTexture = textures[textureIndex];
+	}
+
+	[Command]
+	void CmdPaint(int newTexture)
+	{
+		currentTexture = newTexture;
+	}
+}
