3e3759a9f1b87a9de58f6f96b648bb8e450df7ae
tuckidart <tiagoduarte6@gmail.com>
2016-09-07 19:54:26 -0300

    CAR HEALTH WORKING, NOT VISIBLE YET!

---
+++ Assets/Scripts/Impact_Effects.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Impact_Effects : NetworkBehaviour {
+
+	private GameObject minimap;
+	private GameObject cam;
+
+	[SyncVar]
+	public float carhealth = 100.0f;
+
+	private float damageConstant = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		minimap = GameObject.Find ("MiniMap").gameObject;
+		cam = GameObject.FindGameObjectWithTag ("MainCamera").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision hit)
+	{
+		if (isLocalPlayer)
+		{
+			if (hit.relativeVelocity.magnitude > 10.0f) {
+				minimap.GetComponent<bl_MiniMap> ().DoHitEffect ();
+				carhealth -= damageConstant * hit.relativeVelocity.magnitude;
+			}
+		}
+
+		/*if(carhealth < 0)
+		{
+			destroycar ();
+		}*/
+	}
+}
