f7e68bae0e9cecf205bcfe36ac48fae64556fd7f
tuckidart <tiagoduarte6@gmail.com>
2016-09-09 00:11:50 -0300

    x

--- Assets/Scripts/ObjectivesController.cs
+++ Assets/Scripts/ObjectivesController.cs
@@ -6,13 +6,13 @@ public class ObjectivesController : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		
 	}
 
 	public void CheckObjetiveVariables ()
