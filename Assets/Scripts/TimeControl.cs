9d516749238dc00646b752a53bdba8049a8867a5
guilschaidhauer <MacPro@Homes-MacBook-Pro.local>
2016-09-15 22:00:59 -0300

    Relogio Adicionado

--- Assets/Scripts/TimeControl.cs
+++ Assets/Scripts/TimeControl.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TimeControl : MonoBehaviour {
 
 	public float durationTime;
 
+	[SerializeField]
+	private Text timeText;
 	private float startTime;
 
+	private float totalSeconds;
+	private int minutes;
+	private int seconds;
 
 	// Use this for initialization
-	void Start () 
+	void Awake () 
 	{
 		startTime = Time.time;
-		//StartCoroutine (StartCountdownToEndGame (durationTime));
 	}
 
 	void Update ()
@@ -23,8 +28,8 @@ public class TimeControl : MonoBehaviour {
 		} 
 		else 
 		{
-			//Debug.Log (durationTime - Time.time + startTime);
-		}	
+			UpdateTimeUI ();
+		}
 	}
 
 	private void FireEndOfDurationTime ()
@@ -33,5 +38,21 @@ public class TimeControl : MonoBehaviour {
 		//Time.timeScale = 0;
 
 		_GameMaster.GM.myObjectivesController.DoTimerOverActions ();
+	}
+
+	private void UpdateTimeUI ()
+	{
+		totalSeconds = (durationTime + startTime - Time.time);
+		minutes = (int)(totalSeconds / 60f);
+		seconds = (int)totalSeconds - (minutes * 60);
+
+		if (seconds >= 10) 
+		{
+			timeText.text = minutes.ToString () + ":" + seconds.ToString ();
+		} 
+		else 
+		{
+			timeText.text = minutes.ToString () + ":" + "0" + seconds.ToString ();
+		}
 	}
 }
