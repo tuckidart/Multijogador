c403cab9842c71b5824fbc491ec8323b37e6359c
guilschaidhauer <e.guilherme.castro@gmail.com>
2016-12-11 23:47:52 -0800

    Adicionando delay no scape point

--- Assets/Scripts/ScapePointScript.cs
+++ Assets/Scripts/ScapePointScript.cs
@@ -3,9 +3,31 @@ using System.Collections;
 
 public class ScapePointScript : MonoBehaviour {
 
+	public float secondsToWait;
+
+	private bool hasExited;
+
 	void OnTriggerEnter (Collider other)
 	{
-		if (other.transform.parent.parent.gameObject.tag == "Suspect")
+		if (other.transform.parent.parent.gameObject.tag == "Suspect") 
+		{
+			//_GameMaster.GM.myObjectivesController.CallSupectScaped ();
+			Invoke ("CallSuspectEscaped", secondsToWait);
+			hasExited = false;
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.transform.parent.parent.gameObject.tag == "Suspect") 
+		{
+			hasExited = true;
+		}
+	}
+
+	private void CallSuspectEscaped ()
+	{
+		if (hasExited == false) 
 			_GameMaster.GM.myObjectivesController.CallSupectScaped ();
 	}
 }
